dce33be4407b1c61822f38f06c53de6f7eb6a939
Chris West (Faux) <git@goeswhere.com>
2017-02-12 22:52:31 +0000

    further argument parsing and validation

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,25 @@ extern crate getopts;
 use getopts::Options;
 use std::env;
 
+use std::io::prelude::*;
+use std::fs::File;
+
+fn unarchive(root: &str, blocksize: u64, offset: u64) -> u8 {
+    return 1;
+}
+
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} FILE [options]", program);
     print!("{}", opts.usage(&brief));
 }
 
-fn main() {
+#[derive(PartialEq)]
+enum Operation {
+    Archive,
+    Unarchive,
+}
+
+fn real_main() -> u8 {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
 
@@ -25,7 +38,12 @@ fn main() {
 
     if matches.opt_present("h") {
         print_usage(&program, opts);
-        return;
+        return 2;
+    }
+
+    if !(matches.opt_present("e") ^ matches.opt_present("u")) {
+        print!("-e and -u don't make sense together\n");
+        return 3;
     }
 
     let blocksize: u64 = match matches.opt_str("b") {
@@ -33,5 +51,57 @@ fn main() {
         None => 1 * 1024 * 1024 * 1024
     };
 
-    print!("{}", blocksize)
+    if blocksize <= 16 {
+        print!("blocksize must be >16\n");
+        return 3;
+    }
+
+    let op = if matches.opt_present("u") {
+        Operation::Unarchive
+    } else {
+        Operation::Archive
+    };
+
+    if Operation::Unarchive == op {
+        let offset: u64 = match matches.opt_str("u") {
+            Some(x) => x.parse().unwrap(),
+            None => panic!("unreachable"),
+        };
+
+        if 1 != matches.free.len() {
+            print_usage(&program, opts);
+            return 2;
+        }
+
+        return unarchive(matches.free[0].as_str(), blocksize, offset);
+    }
+
+    if 2 != matches.free.len() {
+        print_usage(&program, opts);
+        return 2;
+    }
+
+    let dest_root = matches.free[0].as_str();
+    let src_path = matches.free[1].as_str();
+
+    let extra = match matches.opt_str("e") {
+        Some(x) => x,
+        None => String::from(""),
+    };
+
+    // read-only by default
+    let mut src = match File::open(src_path) {
+        Ok(x) => x,
+        Err(e) => {
+            print!("src file problem: {}: {}\n", src_path, e);
+            return 4;
+        }
+    };
+
+    print!("{} {}\n", blocksize, extra);
+    return 0;
+}
+
+fn main() {
+    std::process::exit(real_main() as i32);
 }
