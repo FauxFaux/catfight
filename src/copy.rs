a41b01fa136c8886478077c53746940ab2f89331
Chris West (Faux) <git@goeswhere.com>
2017-02-14 20:34:59 +0000

    write headers and unlock; then call copy

---
+++ src/copy.rs
@@ -0,0 +1,5 @@
+use std::fs::File;
+
+pub fn copy_file(src: &File, dest: &File, len: u64) -> Result<(), String> {
+    return Err("NOPE".to_string())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+extern crate byteorder;
 extern crate getopts;
 extern crate libc;
 
-use getopts::Options;
-use std::env;
+mod copy;
 
+// normal, sane imports:
+use std::env;
 use std::io::prelude::*;
 use std::fs;
 use std::fs::File;
 
+// from crates:
+use getopts::Options;
+
+// magic method-adding imports:
 use std::os::unix::io::AsRawFd;
+use byteorder::{BigEndian, WriteBytesExt};
 
 fn unarchive(root: &str, blocksize: u64, offset: u64) -> u8 {
     // TODO
@@ -32,6 +39,7 @@ fn errno() -> i32 {
     }
 }
 
+/// @return true if locked, false if locking would block, error if something went wrong
 fn non_blocking_flock(what: &File) -> Result<bool, i32> {
     unsafe {
         let ret = libc::flock(what.as_raw_fd(), libc::LOCK_EX | libc::LOCK_NB);
@@ -44,6 +52,16 @@ fn non_blocking_flock(what: &File) -> Result<bool, i32> {
         }
     }
     return Ok(true)
+}
+
+fn unlock_flock(what: &File) -> Result<(), i32> {
+    unsafe {
+        let ret = libc::flock(what.as_raw_fd(), libc::LOCK_UN);
+        if 0 != ret {
+            return Err(errno())
+        }
+    }
+    return Ok(())
 }
 
 #[derive(PartialEq)]
@@ -143,20 +161,47 @@ fn real_main() -> u8 {
 
     for target_num in 0..std::u64::MAX {
         let target_path = format!("{}.{:022}", dest_root, target_num);
-        let fd = match File::create(target_path.as_str()) {
-            Ok(x) => x,
-            Err(e) => panic!(e),
-        };
+        let mut fd = File::create(target_path.as_str()).unwrap();
 
-        match non_blocking_flock(&fd) {
-            Ok(locked) => if !locked {
-                skipped_due_to_locking = true;
-                continue;
-            },
-            Err(e) => panic!(e),
-        };
+        if !non_blocking_flock(&fd).unwrap() {
+            skipped_due_to_locking = true;
+            continue;
+        }
 
-        print!("{}\n", target_path);
+        let mut seek: u64 = fd.seek(std::io::SeekFrom::End(0)).unwrap();
+
+        if 0 == seek {
+            // we locked a new file, write a header
+            fd.write_all(b"cf1\0\0\0\0\0").unwrap();
+            fd.write_u64::<BigEndian>(target_num).unwrap();
+            seek = 16;
+        }
+
+        assert!(seek + 16 < (std::i64::MAX as u64));
+
+        if 0 != (seek % 16) {
+            let adjustment: i64 = 16 - (seek % 16) as i64;
+            seek = fd.seek(std::io::SeekFrom::Current(adjustment)).unwrap();
+        }
+
+        if seek >= blocksize {
+            continue;
+        }
+
+        {
+            let extra_len: u64 = extra.len() as u64;
+            let record_end = 8 + 8 + src_len + extra_len;
+            fd.write_u64::<BigEndian>(record_end).unwrap();
+            fd.write_u64::<BigEndian>(extra_len).unwrap();
+            fd.write_all(extra.as_bytes()).unwrap();
+        }
+
+        fd.set_len(seek + src_len).unwrap();
+        unlock_flock(&fd).unwrap();
+
+        copy::copy_file(&src, &fd, src_len).unwrap();
+
+        print!("{}\n", target_num * blocksize + seek);
         break;
     }
 
